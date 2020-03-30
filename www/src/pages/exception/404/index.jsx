67c0a34c07d9e3f1158a72a630f12a37c4c13cfa
九彩拼盘 <iamjoel007@gmail.com>
2020-03-30 09:48:05 +0800

    add 404

---
+++ www/src/pages/exception/404/index.jsx
@@ -0,0 +1,20 @@
+// 这个文件不能删，框架有依赖这个组件...
+import { Link } from 'umi';
+import { Result, Button } from 'antd';
+import React from 'react';
+
+export default () => (
+  <Result
+    status="404"
+    title="404"
+    style={{
+      background: 'none',
+    }}
+    subTitle="Sorry, the page you visited does not exist."
+    extra={
+      <Link to="/">
+        <Button type="primary">Back Home</Button>
+      </Link>
+    }
+  />
+);
